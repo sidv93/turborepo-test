88a768b79955038a067eb35e8d3e3a78415b1fa7
Siddharth Venkatesh <siddhuv93@gmail.com>
2022-05-02 10:34:55 +0530

    linter package change

--- apps/admin/src/App.jsx
+++ apps/admin/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <p>Welcome to Admin app</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
