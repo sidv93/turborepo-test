841df910177d4e2b9eac67e47d556eb48dae7228
Amish Menon <amish.menon@allata.com>
2022-09-15 14:00:12 -0500

    initial babel dependencies -- failing tests

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,3 @@
 module.exports = {
   presets: ["@babel/preset-env"],
-  plugins: [
-    "@babel/plugin-transform-modules-commonjs",
-    ["@babel/plugin-transform-react-jsx", { pragma: "h" }],
-    [
-      "@babel/plugin-transform-runtime",
-      {
-        regenerator: true,
-      },
-    ],
-  ],
 };
